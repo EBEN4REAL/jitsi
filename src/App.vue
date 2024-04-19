c54c74bc731927e1c72e8114c048e55a019e0ef8
eigbinoba <eigbinoba@axxess.com>
2024-04-19 17:38:18 +0100

    made changes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
-<div class="flex h-screen items-center justify-center">
-    <div class="p-10 flex flex-col items-center justify-center">
-        <img class="mb-5" alt="Vue logo" src="./assets/logo.png" />
-        <HelloWorld msg="Vue 3.0 + Vite + TypeScript" />
-    </div>
-</div>
+    <VideoConference />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VideoConference from './components/VideoConference.vue'
 
 export default {
     name: 'App',
     components: {
-        HelloWorld,
+        VideoConference,
     },
 }
 </script>
